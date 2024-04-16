60f08af4069dbed6b6ac7dccc75d41534cac90b8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-16 07:50:40 +0430

    adding screencs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
+    screen: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
