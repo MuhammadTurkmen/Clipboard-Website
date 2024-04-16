68ee5c91bed8f2700fad14fe474be7cf3fe2a1bd
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-04-16 07:54:37 +0430

    adding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
-    extend: {},
+    extend: {
+      colors: {
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
+    },
   },
   plugins: [],
 };
